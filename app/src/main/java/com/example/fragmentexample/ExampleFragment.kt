9ac95abc15db23135dfb506232d2f528ba68a6f7
anuragsingh23 <anuragshekhawat17@gmail.com>
2021-10-23 20:31:00 +0530

    Fragment with lifecycle

---
+++ app/src/main/java/com/example/fragmentexample/ExampleFragment.kt
@@ -0,0 +1,103 @@
+package com.example.fragmentexample
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ExampleFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ExampleFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.i("onCreate","Method is called..")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_example, container, false)
+        Log.i("onCreateView","Method is called..")
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.i("onViewCreate","Method is called..")
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        Log.i("onViewStateRestored","Method is called..")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("onStart","Method is called..")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("onResume","Method is called..")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("onPause","Method is called..")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("onstop","Method is called..")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        Log.i("onSaveInstanceState","Method is called..")    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.i("onDestroyView","Method is called..")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("onDestroy","Method is called..")
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment ExampleFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            ExampleFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
